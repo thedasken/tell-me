0b5440dafbdf43f69a8ccc4ce353b054971cc9a9
thedasken <thedasken@proton.me>
2026-07-17 15:52:19 +0200

    feat: built landing page for a modern and attractive look

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -5,5 +5,11 @@ declare(strict_types=1);
 test('returns a successful response', function () {
     $response = $this->get(route('home'));
 
-    $response->assertOk();
+    $response
+        ->assertOk()
+        ->assertSee(config('app.name'))
+        ->assertSee('Commencer gratuitement')
+        ->assertSee('Se connecter')
+        ->assertSee('resources/css/app.css')
+        ->assertDontSee('Laravel has an incredibly rich ecosystem.');
 });
